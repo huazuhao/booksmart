7f24ca884cb6c1092da866ce926cbd7e6ca7ae05
maxscheinberg <75459032+maxscheinberg@users.noreply.github.com>
2020-12-14 11:24:23 -0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,304 +1,211 @@
-from db import *
-from flask import Flask
-import json
-from flask import request
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+import datetime
+import base64
+import boto3
+from io import BytesIO
+import datetime
+from mimetypes import guess_extension, guess_type
+from PIL import Image
+import random
+import re
+import string
 import os
-import users_dao
+import hashlib
 
-app = Flask(__name__)
-db_filename = "textbook.db"
+db = SQLAlchemy()
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_ECHO"] = True
+EXTENSIONS = ['png','jpg','jpeg']
+BASE_DIR = os.getcwd()
+S3_BUCKET = "booksmart"
+S3_BASE_URL = f"https://{S3_BUCKET}.s3-us-east-1.amazonaws.com"
 
-db.init_app(app)
-with app.app_context():
-    db.create_all()
+book_user_table = db.Table("book_user_table", db.Model.metadata,
+    db.Column("book_id", db.Integer, db.ForeignKey("book.id")),
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"))
+)
 
-def extract_token(request):
-    auth_header = request.headers.get("Authorization")
-    if auth_header is None:
-        return False, json.dumps({"error": "Missing authorization header."})
-    
-    bearer_token = auth_header.replace("Bearer ", "").strip()
-    if bearer_token is None or not bearer_token:
-        return False, json.dumps({"error": "Invalid authorization header."})
+def _get_date():
+    return str(datetime.datetime.now())
 
-    return True, bearer_token 
+class Book(db.Model):
+  __tablename__ = 'book'
+  id = db.Column(db.Integer, primary_key = True)
+  image = db.relationship('Asset', cascade='delete')
+  title = db.Column(db.String, nullable = False)
+  author = db.Column(db.String, nullable = False)
+  courseName = db.Column(db.String, nullable = False)
+  isbn = db.Column(db.String, nullable = False)
+  edition = db.Column(db.String, nullable = False)
+  condition = db.Column(db.String, nullable = False)
+  price = db.Column(db.String, nullable = False)
+  # sellType = db.Column(db.String, nullable = False)
+  available = db.Column(db.Boolean, nullable = False, unique=False, default=True)
 
-def success_response(data, code=200):
-    return json.dumps({"success": True, "data": data}), code
+  createdAt = db.Column(db.String, default=_get_date)
+  updatedAt = db.Column(db.String, onupdate=_get_date)
 
-def failure_response(message, code=404):
-    return json.dumps({"success": False, "error": message}), code
+  sellerId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-######### BOOKS ##########
-@app.route('/api/books/all/')
-def get_books():
-    data = []
-    for b in Book.query.all():
-        mydict = b.serialize()
-        data.append(mydict)
-    return success_response(data)
+  users = db.relationship('User', secondary=book_user_table, back_populates='cart')
 
-@app.route('/api/books/<int:id>/')
-def get_book(id):
-    c = Book.query.filter_by(id = id).first()
-    if c is None:
-        return failure_response('book not found')
-    data = c.serialize()
-    return success_response(data)
+  def __init__(self, **kwargs):
+    self.title = kwargs.get('title')
+    self.author = kwargs.get('author')
+    self.courseName = kwargs.get('courseName')
+    self.isbn = kwargs.get('isbn')
+    self.edition = kwargs.get('edition')
+    self.price = kwargs.get('price')
+    # self.sellType = kwargs.get('sellType') # TODO: validate
+    self.sellerId = kwargs.get('sellerId')
+    self.condition = kwargs.get('condition')
 
-@app.route('/api/books/sell/', methods=["POST"])
-def create_book():
-    '''
-    body:
-    [required]: title, price, sellerId
-    [optional]: image, author, courseName, isbn, edition
-    '''
-    was_successful, session_token = extract_token(request)
+  def serialize(self):
+    return {
+      'id': self.id,
+      'image': [i.serialize() for i in self.image],
+      'title': self.title,
+      'author':self.author,
+      'courseName': self.courseName,
+      'isbn': self.isbn,
+      'edition': self.edition,
+      'price': self.price,
+      # 'sellType': self.sellType,
+      'available': self.available,
+      'condition': self.condition,
 
-    if not was_successful:
-        return session_token
+      'createdAt': self.createdAt,
+      'updatedAt': self.updatedAt,
 
-    user = users_dao.get_user_by_update_token(session_token)
-    if not user or not user.verify_update_token(session_token):
-        return json.dumps({"error": "Invalid update token."})
-    
-    body = json.loads(request.data)
-    price = body.get('price')
+      'sellerId': self.sellerId
+    }
+  
+class User(db.Model):
+  __tablename__ = 'user'
+  id = db.Column(db.Integer, primary_key = True)
+  email = db.Column(db.String, nullable = False)
+  password_digest = db.Column(db.String, nullable=False)
+  session_token = db.Column(db.String, nullable=False, unique=True)
+  session_expiration = db.Column(db.DateTime, nullable=False)
+  update_token = db.Column(db.String, nullable=False, unique=True)
+  name = db.Column(db.String, nullable = False)
+  selling = db.relationship('Book', cascade='delete')
+  cart = db.relationship('Book', secondary=book_user_table, back_populates='users')
 
-    # check necessary fields
-    if body.get('title') is None or price is None or not (isinstance(price, int) or \
-        isinstance(price, float)) or body.get('sellerId') is None:
-        return failure_response('title/price/sellerId is empty or malformed')
+  def __init__(self, **kwargs):
+    self.email = kwargs.get('email')
+    self.name = kwargs.get('name')
+    self.password_digest = bcrypt.hashpw(kwargs.get("password").encode("utf8"), bcrypt.gensalt(rounds=13))
+    self.renew_session()
 
-    # check user exists
-    c = User.query.filter_by(id = body.get('sellerId')).first()
-    if c is None:
-        return failure_response('user not found')
+  def serialize(self):
+    return{
+      'id': self.id,
+      'email': self.email,
+      'name': self.name,
+      'selling': [b.serialize() for b in self.selling],
+      'cart': [b.serialize() for b in self.cart]
+    }
 
-    # check user is the one who logged in
-    if user != c:
-        return failure_response('Id does not match token')
+  def is_selling(self,book):
+    return book in self.selling
 
-    # create new book
-    new_book = Book(image=body.get('image',''), title=body.get('title'),\
-        author=body.get('author',''), courseName=body.get('courseName',''),\
-            isbn=body.get('isbn',''), edition=body.get('edition',''),condition=body.get('condition',''),\
-            price=str(round(price, 2)),sellerId=body.get('sellerId'))
-    db.session.add(new_book)
-    db.session.commit()
-    data = new_book.serialize()
+    # Used to randomly generate session/update tokens
+  def _urlsafe_base_64(self):
+      return hashlib.sha1(os.urandom(64)).hexdigest()
 
-    return success_response(data, 201)
+  # Generates new tokens, and resets expiration time
+  def renew_session(self):
+      self.session_token = self._urlsafe_base_64()
+      self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=1)
+      self.update_token = self._urlsafe_base_64()
 
-@app.route('/api/dev/books/delete-all/', methods=["DELETE"])
-def clear_books():
-    db.session.query(Book).delete()
-    db.session.commit()
-    return success_response({})
+  def verify_password(self, password):
+      return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
 
-######### USERS ##########
-@app.route('/api/users/<int:id>/')
-def get_user(id):
-    c = User.query.filter_by(id = id).first()
-    if c is None:
-        return failure_response('user not found')
-    data = c.serialize()
-    return success_response(data)
+    # Checks if session token is valid and hasn't expired
+  def verify_session_token(self, session_token):
+      return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
 
-# @app.route('/api/users/', methods=["POST"])
-# def create_user():
-#     '''
-#     body:
-#     [required]: name, email
-#     '''
-#     body = json.loads(request.data)
-#     if body.get('name') is None or body.get('email') is None:
-#         return failure_response('name or email is empty')
-#     new_user = User(name = body.get('name'), email = body.get('email'))
-#     db.session.add(new_user)
-#     db.session.commit()
-#     data = new_user.serialize()
-#     return success_response(data, 201)
+  def verify_update_token(self, update_token):
+      return update_token == self.update_token
 
-@app.route("/api/register/", methods=["POST"])
-def register_account():
-    body = json.loads(request.data)
-    email = body.get("email")
-    password = body.get("password")
-    name = body.get("name")
+class Asset(db.Model):
+  __tablename__ = 'asset'
 
-    if email is None or password is None or name is None:
-        return json.dumps({"error": "Invalid email, name, or password"})
+  id = db.Column(db.Integer, primary_key=True)
+  base_url = db.Column(db.String, nullable=True)
+  salt = db.Column(db.String, nullable=True)
+  extension = db.Column(db.String, nullable=True)
+  width = db.Column(db.Integer, nullable=True)
+  height = db.Column(db.Integer, nullable=True)
+  # createdAt = db.Column(db.String, default=_get_date)
+  createdAt = db.Column(db.DateTime, nullable=False)
 
-    was_created, user = users_dao.create_user(email, password, name)
+  bookId = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
 
-    if not was_created:
-        return json.dumps({"error": "User already exists"})
+  def __init__(self, **kwargs):
+    self.create(kwargs.get('imageData'))
+    self.bookId = kwargs.get('bookId')
 
-    return json.dumps({
-        "session_token": user.session_token,
-        "session_expiration": str(user.session_expiration),
-        "update_token": user.update_token
-        })
+  def serialize(self):
+    return{
+      'bookId': self.bookId,
+      'url': f'{self.base_url}/{self.salt}.{self.extension}',
+      'createdAt': str(self.createdAt)
+    }
 
-@app.route("/api/login/", methods=["POST"])
-def login():
-    body = json.loads(request.data)
-    email = body.get("email")
-    password = body.get("password")
-
-    if email is None or password is None:
-        return json.dumps({"error": "Invalid email or password"})
-
-    was_successful, user = users_dao.verify_credentials(email, password)
-
-    if not was_successful:
-        return json.dumps({"error": "Incorrect email or password."})
-
-    return json.dumps({
-        "session_token": user.session_token,
-        "session_expiration": str(user.session_expiration),
-        "update_token": user.update_token
-        })
-
-@app.route("/api/session/", methods=["POST"])
-def update_session():
-    was_successful, update_token = extract_token(request)
-
-    if not was_successful:
-        return update_token
-        
+  def create(self, imageData):
     try:
-        user = users_dao.renew_session(update_token)
+      #base64 string -> .png -> png
+      ext = guess_extension(guess_type(imageData)[0])[1:]
+      if ext not in EXTENSIONS:
+        raise Exception(f'Extension {ext} not supported.')
+
+      # generating random string for image name
+      salt = "".join(random.SystemRandom().choice(
+        string.ascii_uppercase + string.digits
+        )
+        for _ in range(16)
+      )
+
+      # remove header of base64 string and open image
+      img_str = re.sub("^data:image/.+;base64,","",imageData)
+      img_data = base64.b64decode(img_str)
+      img = Image.open(BytesIO(img_data))
+
+      self.base_url = S3_BASE_URL
+      self.salt = salt
+      self.extension = ext
+      self.width = img.width
+      self.height = img.height
+      self.createdAt = datetime.datetime.now()
+
+      img_filename = f'{salt}.{ext}'
+      self.upload(img, img_filename)
+
     except Exception as e:
-        return json.dumps({"error": f"Invalid update token: {str(e)}"})
+      print(f'Unable to create image due to {e}.')
 
-    return json.dumps({
-        "session_token": user.session_token,
-        "session_expiration": str(user.session_expiration),
-        "update_token": user.update_token
-        })
+  def upload(self, img, img_filename):
+    try:
+      # save image temporarily on server
+      img_temploc = f'{BASE_DIR}/{img_filename}'
+      img.save(img_temploc)
 
-@app.route('/api/users/<int:id>/cart/add/', methods=["POST"])
-def add_to_cart(id):
-    '''
-    body:
-    [required]: bookId
-    '''
-    was_successful, session_token = extract_token(request)
+      # upload image to S3
+      s3_client = boto3.client('s3')
+      s3_client.upload_file(img_temploc, S3_BUCKET, img_filename)
 
-    if not was_successful:
-        return session_token
+      # make S3 img url public
+      s3_resource = boto3.resource('s3')
+      object_acl = s3_resource.ObjectAcl(S3_BUCKET, img_filename)
+      object_acl.put(ACL="public-read")
 
-    user = users_dao.get_user_by_update_token(session_token)
-    if not user or not user.verify_update_token(session_token):
-        return json.dumps({"error": "Invalid session token."})
-    
-    body = json.loads(request.data)
-    bookId = body.get('bookId')
-
-    if bookId is None:
-        return failure_response('bookId is empty')
-
-    # get book
-    book = Book.query.filter_by(id = bookId).first()
-    if book is None:
-        return failure_response('book not found')
-    
-    # get user
-    c = User.query.filter_by(id = id).first()
-    if c is None:
-        return failure_response('user not found')
-
-    # check user is the one who logged in
-    if user != c:
-        return failure_response('Id does not match token')
-
-    #TODO: not your own book
-    if user.is_selling(book):
-        return failure_response('Cannot add own book to cart.')
-
-    # add to cart
-    assoc = book_user_table.insert().values(book_id=bookId, user_id=id)
-    db.session.execute(assoc)
-    db.session.commit()
-
-    return success_response(book.serialize(), 201)
-
-@app.route('/api/users/<int:id>/cart/remove/', methods=['DELETE'])
-def remove_from_cart(id):
-    '''
-    body:
-    [required]: bookId
-    '''
-    was_successful, session_token = extract_token(request)
-
-    if not was_successful:
-        return session_token
-
-    user = users_dao.get_user_by_update_token(session_token)
-    if not user or not user.verify_update_token(session_token):
-        return json.dumps({"error": "Invalid session token."})
-    
-    body = json.loads(request.data)
-    bookId = body.get('bookId')
-
-    if bookId is None:
-        return failure_response('name or email is empty')
-
-    # get book
-    book = Book.query.filter_by(id = bookId).first()
-    if book is None:
-        return failure_response('book not found')
-    
-    # get user
-    c = User.query.filter_by(id = id).first()
-    if c is None:
-        return failure_response('user not found')
-
-    # check user is the one who logged in
-    if user != c:
-        return failure_response('Id does not match token')
-
-    # remove from cart
-    if book in user.cart:
-        user.cart.remove(book)
-        db.session.commit()
-
-    return success_response(book.serialize(), 201)
+      # remove from temporary location
+      os.remove(img_temploc)
 
 
-@app.route('/api/dev/users/delete-all/', methods=['DELETE'])
-def clear_users():
-    db.User.query.delete()
-    return success_response({})
+    except Exception as e:
+      print(f'Unable to create image due to {e}.')
 
-###### ASSET #######
-@app.route('/api/upload/', methods=['POST'])
-def upload():
-    body = json.loads(request.data)
-    imageData = body.get('imageData')
-    bookId = body.get('bookId')
-
-    # get book
-    book = Book.query.filter_by(id = bookId).first()
-    if book is None:
-        return failure_response('book not found (create the book first!)')
-
-    if imageData is None:
-        return failure_response('No base64 URL to be found.')
-
-    asset = Asset(imageData=imageData, bookId=bookId)
-    db.session.add(asset)
-    db.session.commit()
-    
-    return success_response(asset.serialize(), 201)
-
-if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
-    app.run(host="0.0.0.0", port=5000, debug=True)
